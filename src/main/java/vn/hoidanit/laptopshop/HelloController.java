b8d3ee13801fc2c969413b34f0e1fb24cf9a58c9
Quang Chan <chan09012004@gmail.com>
2025-06-18 21:45:55 +0700

    Udate Code 9:45 18/06

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -7,6 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Hello World from C7A13";
+        return "Hello World from C7A13 !!";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only user";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only Admin";
     }
 }
