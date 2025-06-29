ca32d3503061ce95cf52bfdbdaf207666b5c8ac3
Quang Chan <chan09012004@gmail.com>
2025-06-30 00:08:25 +0700

    MVC upadate

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,22 +1,13 @@
 package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import vn.hoidanit.laptopshop.service.UserService;
 
-// @Controller
-// public class UserController {
-//         @RequestMapping("/")
-//     private String getHomePage(){
-//         return "hello from controller";
-//     }
-
-// }
-
-@RestController
+@Controller
 public class UserController {
 
     private UserService userService;
@@ -25,9 +16,27 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("")
-    private String getHomePage() {
-        return this.userService.handleHello();
+    @RequestMapping("/")
+    private String getHomePage(Model model) {
+        String test = this.userService.handleHello();
+        model.addAttribute("chan", test);
+        return "hello";
     }
 
 }
+
+// @RestController
+// public class UserController {
+
+// private UserService userService;
+
+// public UserController(UserService userService) {
+// this.userService = userService;
+// }
+
+// @GetMapping("")
+// private String getHomePage() {
+// return this.userService.handleHello();
+// }
+
+// }
